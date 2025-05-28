b7fe85e56216ae237ddb780b4f906f9c900420c2
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-05-28 20:38:37 -0300

    Exercício_06

---
+++ src/exercicio_06/Exercicio_06.java
@@ -0,0 +1,16 @@
+package exercicio_06;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class Exercicio_06 {
+
+    public static void main(String[] args) {
+        int numero;
+        numero = 10;
+        JOptionPane.showMessageDialog(null, "O valor da variável é : " + numero);
+    }
+}
